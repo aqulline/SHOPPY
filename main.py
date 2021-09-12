515201591d173b0b0d92c0df4cfb658290b649a5
beast <aqegliapmbuya@gmail.com>
2021-09-13 01:19:28 +0300

    And here comes comfort at least now

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.label import MDLabel, MDIcon
 
 if utils.platform != 'android':
-
     Window.size = (360, 640)
 
 
@@ -65,10 +64,10 @@ class MainApp(MDApp):
     def backgrounds_colors(self):
         toolbar = self.root.ids.tool
         toolbar.md_bg_color = 1, 1, 1, 1
-        toolbar.specific_text_color = 83/225, 186/225, 115/225, 1
+        toolbar.specific_text_color = 83 / 225, 186 / 225, 115 / 225, 1
 
         nav = self.root.ids.bnav
-        nav.text_color_active = 83/225, 186/225, 115/225, 1
+        nav.text_color_active = 83 / 225, 186 / 225, 115 / 225, 1
 
     def spin_dialog(self):
         if not self.dialog_spin:
@@ -82,13 +81,23 @@ class MainApp(MDApp):
     def spin_dismiss(self):
         self.dialog_spin.dismiss()
 
+    ''''
+    
+                DOWN HERE STAYS ONLY TESTING FUNCTIONS
+    
+    '''
+
+    def desc(self):
+        rt = self.root
+        rt.current = 'description'
+
     def first(self):
         self.spin_dialog()
         Clock.schedule_once(lambda x: self.test(), 4)
 
     def test(self):
         for i in range(9):
-            card = Foods()
+            card = Foods(on_release=lambda x: self.desc())
             scroll = self.root.ids.front_shop
             self.product_name = f'product{i}'
             self.product_price = f'price{i}/tsh'
