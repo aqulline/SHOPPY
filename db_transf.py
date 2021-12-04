b0d925f5840abcd53aa004ed2e11d677dd3fe5e4
aqulline <aqeglipambuya@gmail.com>
2021-12-05 00:45:54 +0300

    order already created you will thanks me later you fucker
    aqulline

--- db_transf.py
+++ db_transf.py
@@ -8,6 +8,9 @@ class Transfer:
     date, time = current_time.strip().split()
     week_day = ""
     day = ""
+    orders = '1'
+    number = 0
+    order_id = '123'
 
     def register(self, phone, username):
         if True:
@@ -32,3 +35,80 @@ class Transfer:
                         'birth-date': 'None',
 
                     })
+
+    def Order(self, company, phone, location, quantity, amount, product_name):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+
+            self.orders_calc(company, product_name)
+            self.current_time = str(datetime.datetime.now())
+            self.date, self.time = self.current_time.strip().split()
+            self.id_generator()
+            ref = db.reference('Shoppy').child('Company').child(company).child("Orders").child(product_name). \
+                child(self.order_id)
+            ref.set({
+                "company_phone": company,
+                "Phone number": phone,
+                "location": location,
+                "quantity": quantity,
+                "amount": amount,
+                "oder_id": self.order_id,
+                "product name": product_name,
+                "time": self.time,
+                "date": self.date
+            })
+            self.day_calc()
+            refe = db.reference('Shoppy').child('Company').child(company).child("statistics"). \
+                child(self.day).child(product_name)
+            refe.set({
+                "orders": self.orders
+            })
+
+    def orders_calc(self, company, name):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+
+            print('now crash you fucker')
+            self.day_calc()
+            try:
+                print("Hell no")
+                ref = db.reference('Shoppy').child('Company').child(company).child('statistics').child(self.day).child(
+                    name).child('orders')
+                self.orders = str(ref.get())
+                self.orders = str(int(self.orders) + 1)
+                print(self.orders)
+            except:
+                self.orders = '1'
+
+    def day_calc(self):
+        today = str(datetime.date.today())
+        y, mon, day = today.strip().split("-")
+
+        print(f'{y}-{mon}-{day}')
+        time = f'{y}-{mon}-{day}'
+        time = time
+        self.day = time
+
+    def id_generator(self):
+        date1 = datetime.datetime.now()
+        date, time = id = str(date1).split(" ")
+        admin_product_id = str(date + time)
+        new_id = admin_product_id.replace('-', '')
+        new_id = new_id.replace(':', '')
+        new_id = new_id.replace('.', '')
+        self.order_id = new_id
+
+
+company, phone, location, quantity, amount, product_name = '0687863886', '0788204327', 'arusha', '5', '200', 'dildo'
+
+Transfer.Order(Transfer(), company, phone, location, quantity, amount, product_name)
--- playg/let.py
+++ playg/let.py
@@ -1,4 +1,23 @@
-from string import ascii_uppercase
+import datetime
 
-for i in ascii_uppercase:
-    print(i+'.png')
+
+def day_calc():
+    today = str(datetime.date.today())
+    y, mon, day = today.strip().split("-")
+
+    print(f'{y}-{mon}-{day}')
+    time = f'{y}-{mon}-{day}'
+    return time
+
+
+def id_generator():
+    date1 = datetime.datetime.now()
+    date, time = id = str(date1).split(" ")
+    admin_product_id = str(date + time)
+    new_id = admin_product_id.replace('-', '')
+    new_id = new_id.replace(':', '')
+    new_id = new_id.replace('.', '')
+    print(new_id)
+
+
+
